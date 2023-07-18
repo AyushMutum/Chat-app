13b883057b1f3f6f737e4d58f66d7790b6b98544
Ayush Mutum <ayushmutum6@gmail.com>
2023-07-18 20:41:27 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
+import React from "react";
 import { ChatEngine } from "react-chat-engine";
 import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
-import './App.css'
+import './App.css';
 
-const App = ( ) => {
-    if (!localStorage.getItem('username')) return <LoginForm />;
+const App = () => {
+ const handleLogout = () => {
+    // Clear username and password from localStorage
+    localStorage.removeItem('username');
+    localStorage.removeItem('password');
+    // Reload the page to show the login form again
+    window.location.reload();
+  };
 
-    return(
-        <ChatEngine
+  // Check if the user is logged in by checking the existence of username in localStorage
+  if (!localStorage.getItem('username')) {
+    return <LoginForm />;
+  }
+
+  return (
+    <div className="chat-card-wrap">
+      {/* Add a logout button */}
+      <button className="logout-button" onClick={handleLogout}>Logout</button>
+      <ChatEngine
         height="100vh"
-        projectID="
-        227f55b0-7e1a-49dd-9fc6-5bd08b7177b5"
-        // username of currently log in 
+        projectID="227f55b0-7e1a-49dd-9fc6-5bd08b7177b5"
         userName={localStorage.getItem('username')}
-        // userpassword
         userSecret={localStorage.getItem('password')}
         renderChatFeed={(ChatAppProps) => <ChatFeed {...ChatAppProps} />}
-        />
-    )
+        
+      />
+      
+    </div>
+  );
 }
 
 export default App;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,20 +1,54 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
 const MyMessage = ({ message }) => {
-    if (message.attachments && message.attachments.length > 0) {
-      return (
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false);
+
+  const openImageModal = () => {
+    setIsImageModalOpen(true);
+  };
+
+  const closeImageModal = () => {
+    setIsImageModalOpen(false);
+  };
+
+  if (message.attachments && message.attachments.length > 0) {
+    const imageUrl = message.attachments[0].file;
+
+    return (
+      <>
         <img
-          src={message.attachments[0].file}
+          src={imageUrl}
           alt="message-attachment"
           className="message-image"
-          style={{ float: 'right' }}
+          style={{ float: 'right', cursor: 'pointer' }}
+          onClick={openImageModal}
         />
-      );
-    }
-  
-    return (
-      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
-        {message.text}
-      </div>
+
+        {/* Modal to display the enlarged image */}
+        <Modal
+          isOpen={isImageModalOpen}
+          onRequestClose={closeImageModal}
+          contentLabel="Enlarged Image"
+          className="image-modal"
+          overlayClassName="image-modal-overlay"
+        >
+          <img src={imageUrl} alt="enlarged-message-attachment" className="enlarged-message-image" />
+          <div className="download-button-container">
+            <a href={imageUrl} download>
+              <button className="download-button">Download</button>
+            </a>
+          </div>
+        </Modal>
+      </>
     );
-  };
-  
-  export default MyMessage;
+  }
+
+  return (
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+      {message.text}
+    </div>
+  );
+};
+
+export default MyMessage;
